acb5ed21e0bcbc85b6c275c9b97f882f0fdf1ac0
RdotSilva <RyandotSilva@gmail.com>
2019-12-08 11:02:50 -0500

    Fix typos

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -4,11 +4,13 @@ import { createStage } from "./../utils/gameHelpers";
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+import { StyledTetrisWrapper } from "./styles/StyledTetris";
+import { StyledTetris } from "./styles/StyledTetris";
 
 const Tetris = () => {
   return (
-    <StyledTestrisWrapper>
-      <StyledTestris>
+    <StyledTetrisWrapper>
+      <StyledTetris>
         <Stage stage={createStage()} />
         <aside>
           <div>
@@ -18,8 +20,8 @@ const Tetris = () => {
           </div>
           <StartButton />
         </aside>
-      </StyledTestris>
-    </StyledTestrisWrapper>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
