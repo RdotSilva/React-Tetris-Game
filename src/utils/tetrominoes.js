23ca1091e012f32298d9d7047fcb1879d90c959f
RdotSilva <RyandotSilva@gmail.com>
2019-12-07 19:51:47 -0500

    Add I property

--- src/utils/tetrominoes.js
+++ src/utils/tetrominoes.js
@@ -1,3 +1,12 @@
 export const TETROMINOES = {
-  0: { shape: [[0]], color: "0, 0, 0" }
+  0: { shape: [[0]], color: "0, 0, 0" },
+  I: {
+    shape: [
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0]
+    ],
+    color: "80, 227, 230"
+  }
 };
