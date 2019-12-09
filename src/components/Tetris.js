495e57fb5070641d6c379cf58c278cc4f0947956
RdotSilva <RyandotSilva@gmail.com>
2019-12-09 12:52:22 -0500

    Add state to Tetris component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,13 +1,20 @@
-import React from "react";
-import { createStage } from "./../utils/gameHelpers";
+import React, { useState } from "react";
 
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+
 import { StyledTetrisWrapper } from "./styles/StyledTetris";
 import { StyledTetris } from "./styles/StyledTetris";
 
+import { usePlayer } from "./../hooks/usePlayer";
+import { useStage } from "./../hooks/useStage";
+
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  console.log("Re-Render");
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
