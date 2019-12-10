6ee65afbde4808400759d0623fb93d0bf83b503e
RdotSilva <RyandotSilva@gmail.com>
2019-12-10 10:55:53 -0500

    Add useEffect hook

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createStage } from "../utils/gameHelpers";
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    // Clear stage
+    const updateStage = prevStage => {
+      const newStage = prevStage.map(row =>
+        row.map(cell => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+
+      return newStage;
+    };
+
+    setStage(prev => updateStage(prev));
+  }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
 
   return [stage, setStage];
 };
