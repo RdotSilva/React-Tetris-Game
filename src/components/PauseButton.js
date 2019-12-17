3356c5f609711ac81f848b5590af29fd52acc487
RdotSilva <RyandotSilva@gmail.com>
2019-12-17 09:13:13 -0500

    Add PauseButton component

---
+++ src/components/PauseButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledPauseButton } from "./styles/StyledPauseButton";
+
+const PauseButton = ({ callback, text }) => (
+  <StyledPauseButton onClick={callback}>{text}</StyledPauseButton>
+);
+
+export default PauseButton;
