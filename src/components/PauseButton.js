50a21a7aff8022974427427ea5df8920d9c036a8
RdotSilva <RyandotSilva@gmail.com>
2019-12-17 09:22:33 -0500

    Add gamePaused props to PauseButton

--- src/components/PauseButton.js
+++ src/components/PauseButton.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { StyledPauseButton } from "./styles/StyledPauseButton";
 
-const PauseButton = ({ callback, text }) => (
-  <StyledPauseButton onClick={callback}>{text}</StyledPauseButton>
+const PauseButton = ({ gamePaused, callback, text }) => (
+  <StyledPauseButton gamePaused={gamePaused} onClick={callback}>
+    {text}
+  </StyledPauseButton>
 );
 
 export default PauseButton;
